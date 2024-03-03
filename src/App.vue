89082a111e6170f9e6193f9c172211cc974ba10c
Nam Nguyen <nam.nguyen@shopify.com>
2024-03-03 14:18:00 -0400

    Add spin stuff 💞

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,142 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, computed } from "vue";
+import { useRafFn } from "@vueuse/core";
+
+const MAX_PIXELS_MOVED_PER_FRAME = 10;
+const MIX_PIXELS_MOVED_PER_FRAME = 0;
+
+const SLOW_DOWN_FACTOR = 5;
+const INTERVAL_TIME = 100;
+const SLOW_DOWN_TIME =
+  INTERVAL_TIME * SLOW_DOWN_FACTOR * (1000 / INTERVAL_TIME);
+
+const translateX = ref(0);
+const direction = ref<"left" | "right">("left");
+
+const list = ref<HTMLDivElement>();
+const pixelsMovedPerFrame = ref(MIX_PIXELS_MOVED_PER_FRAME);
+const state = ref<"idle" | "moving" | "slowing">("idle");
+const result = ref<string | null>(null);
+
+useRafFn(() => {
+  const bound = list.value?.clientWidth;
+
+  if (bound) {
+    const newTranslateX = translateX.value + pixelsMovedPerFrame.value;
+
+    if (newTranslateX > bound) {
+      translateX.value = 0;
+    } else {
+      translateX.value = newTranslateX;
+    }
+  }
+});
+
+const style = computed(() => {
+  const bound = list.value?.clientWidth || 0;
+  const newValue =
+    direction.value === "left" ? -translateX.value : translateX.value - bound;
+  return {
+    transform: `translateX(${newValue}px)`,
+  };
+});
+
+const uniqueId = (() => {
+  let id = 0;
+  return () => ++id;
+})();
+
+const images: { src: string; id: number }[] = Array.from(
+  { length: 8 },
+  (_, index) => ({ src: `/${index + 1}.jpeg`, id: uniqueId() })
+);
+
+let currentInterval: number | null = null;
+
+const getResult = () => {
+  const allImages = Array.from(document.querySelectorAll("img") || []);
+
+  for (const image of allImages) {
+    const rect = image.getBoundingClientRect();
+    // Check image in the middle of the screen
+    if (
+      rect.left < window.innerWidth / 2 &&
+      rect.right > window.innerWidth / 2
+    ) {
+      result.value = image.src;
+      console.log("result", result.value);
+      return;
+    }
+  }
+};
+
+const handleClick = () => {
+  if (state.value === "idle") {
+    state.value = "moving";
+    pixelsMovedPerFrame.value = MAX_PIXELS_MOVED_PER_FRAME;
+    return;
+  }
+
+  if (state.value === "moving") {
+    state.value = "slowing";
+
+    currentInterval = setInterval(() => {
+      pixelsMovedPerFrame.value -=
+        (INTERVAL_TIME / SLOW_DOWN_TIME) * MAX_PIXELS_MOVED_PER_FRAME;
+
+      if (pixelsMovedPerFrame.value <= 0) {
+        state.value = "idle";
+        pixelsMovedPerFrame.value = MIX_PIXELS_MOVED_PER_FRAME;
+        currentInterval && clearInterval(currentInterval);
+
+        getResult();
+      }
+    }, INTERVAL_TIME);
+  }
+};
+
+const buttonTextLookup = {
+  idle: "Start",
+  moving: "Stop",
+  slowing: "Slowing",
+};
+</script>
 
 <template>
-  <h1 class="text-red">hello</h1>
+  <div class="m-auto flex flex-col items-center px-4">
+    <h1 class="my-10 text-4xl font-bold">🚀 Demo Marquee 🚀</h1>
+    <button
+      class="text-dark-700 bg-gray-100 decoration-none cursor-pointer rounded-md px-3 py-2 text-sm font-medium"
+      :disabled="state === 'slowing'"
+      @click="handleClick"
+    >
+      {{ buttonTextLookup[state] }}
+    </button>
+    <div class="relative overflow-hidden w-full md:w-8/12 cursor-pointer">
+      <div class="flex will-change-transform" :style="style">
+        <div ref="list" class="flex flex-shrink-0 py-4 relative">
+          <img
+            v-for="image in images"
+            :key="image.id"
+            :src="image.src"
+            class="h-40 w-40"
+          />
+        </div>
+
+        <!-- Cloned stuff -->
+        <div class="flex flex-shrink-0 py-4 relative">
+          <img
+            v-for="image in images"
+            :key="image.id"
+            :src="image.src"
+            class="h-40 w-40"
+          />
+        </div>
+      </div>
+
+      <div
+        class="absolute top-0 bottom-0 z-1 w-0.5 bg-red-500 left-[50%] translate-x-[50%]"
+      ></div>
+    </div>
+  </div>
 </template>
