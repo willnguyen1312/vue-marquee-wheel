7eb51daf880ee9d0d87ea2da2c3db99047b4bfa3
Nam Nguyen <nam.nguyen@shopify.com>
2024-03-04 10:01:06 -0400

    PWA FTW 💞

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,40 @@
 import UnoCSS from "unocss/vite";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  plugins: [UnoCSS(), vue()],
+  plugins: [
+    UnoCSS(),
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+      manifest: {
+        name: "Vue Marquee",
+        short_name: "Marquee",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   build: {
     sourcemap: true,
   },
